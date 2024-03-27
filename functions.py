ebafc033256da586b53b9f19552d70e328a78f6f
Roman Shrestha <romanstha07@gmail.com>
2024-03-26 23:44:50 -0700

    - feat(dataset cleaning): replaced data, removed redundant/missing data, renamed columns and some initial analysis

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
-def col_info(data, col_ids=None, truncate=False):
+def col_info(data, col_ids=None):
     """
-    The function takes a dataset and a list of columns supplied by the user. If only a dataset is passed, the function will default to providing info on all the columns. The columns are specified as either all "labels" or all indices and can be in any desired order. Column indices can be passed using either a range function (for a contiguous list of columns) or a simple list (for non-contiguous columns). If "labels" are specified, these are converted into indices by the function before use.\
-    \n\nEXAMPLES: Suppose the dataset has 10 columns labeled "A" to "J". \n\nSo for all columns use:
-    col_info(range(0, df.shape[1]))\n\nFor the first 6 columns use:
-    col_info(range(0, 5))\n\nFor columns 1, 3, 7, and 8 use:
-    col_info([1, 3, 7, 8])\n\nFor columns "A", "D" and "F" use:
-    col_info(["A", "D", "F"])
+    The function takes a dataset and a list of columns supplied by the user. If only a dataset is passed,
+    the function will default to providing info on all the columns. The columns are specified as either all "labels"
+    or all indices and can be in any desired order. Column indices can be passed using either a range function (for a
+    contiguous list of columns) or a simple list (for non-contiguous columns). If "labels" are specified,
+    these are converted into indices by the function before use. \n\nEXAMPLES: Suppose the dataset has 10 columns
+    labeled "A" to "J". \n\nSo for all columns use: col_info(range(0, df.shape[1]))\n\nFor the first 6 columns use:
+    col_info(range(0, 5))\n\nFor columns 1, 3, 7, and 8 use: col_info([1, 3, 7, 8])\n\nFor columns "A", "D" and "F"
+    use: col_info(["A", "D", "F"])
     """
 
     if col_ids is None:
@@ -24,10 +26,7 @@ def col_info(data, col_ids=None, truncate=False):
     print("The full dataset contains:", data.shape[0], "rows and", data.shape[1],
           "columns. Details for the requested column(s)", "are as follows:\n")
 
-    col_indices_to_use = col_indices  # col_indices_to_use will use all indices without truncate=True
-
-    if truncate:
-        col_indices_to_use = col_indices[:5]  # set col_indices_to_use to first 5 column indices only
+    col_indices_to_use = col_indices  # col_indices_to_use will use all indices
 
     for col in col_indices_to_use:  # for each index in the col_indices_to_use list
         uniques_arr = data.iloc[:, col].unique()
@@ -53,6 +52,3 @@ def col_info(data, col_ids=None, truncate=False):
 
         if (total_length < 50) & (total_values < 12):
             print(values_list, "\n")
-
-    if (truncate == True) & (len(col_indices) > len(col_indices_to_use)):
-        print("... OUTPUT TRUNCATED ... remove truncate=True to display info about all columns")
